2ac8026c635d7b5197497ee8c405c6e698fb7ee6
Ben Poppy <me@benpoppy.dev>
2026-03-23 10:30:22 +0000

    Fix playbook to run from correct directory

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -102,7 +102,7 @@ EOF
   run_step "Setting up repository" "Set up repository" "setup_repo"
   run_step "Installing Ansible deps" "Installed Ansible deps" "install_ansible_deps"
 
-  run_step "Running playbook" "Ran playbook" "ansible-playbook main.yml -i inventory"
+  run_step "Running playbook" "Ran playbook" "cd $INSTALL_DIR && ansible-playbook main.yml -i inventory"
 
   log_success
 }
